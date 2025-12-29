49977ed52bbd93a7b12aca3aab42f75b7896c07f
madhan-lab <smmkw1303@gmail.com>
2025-12-29 22:59:08 +0530

    updating start container script

--- day-14/simple-python-app/start_container.sh
+++ day-14/simple-python-app/start_container.sh
@@ -2,7 +2,7 @@
 set -e
 
 # Pull the Docker image from Docker Hub
-echo
+docker pull madhanhub/simple-python-flask-app:latest
 
 # Run the Docker image as a container
-echo
+docker run -d -p 5000:5000 madhanhub/simple-python-flask-app:latest
